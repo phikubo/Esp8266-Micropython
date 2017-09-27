48eff6738287d411fc797f89bcdc28ef45b2b3c7
Michael Tenorio <31525189+phikubo@users.noreply.github.com>
2017-09-26 22:33:11 -0500

    Update socket_led.py

--- Micropython/NetWorking/socket_led.py
+++ Micropython/NetWorking/socket_led.py
@@ -1,16 +1,17 @@
- 
+#El siguiente script describe una red wifi propia (local) en la cual usando sockets y Gpio, se enciende
+#desde una Ip conocida, un led.
+
 import machine
 import socket
 import time
 import network
-
+##creacion de la red wifi local
 mired=network.WLAN(network.AP_IF)
 mired.config(essid="prueba_de_red_local", authmode=network.AUTH_WPA_WPA2_PSK, password="123456789")
 mired.active(True)
-#print(mired.ifconfig('ip'))
-#print(mired.ifconfig('gateway'))
 print('configuracion total: ',mired.ifconfig())
 
+#html para encender el led.
 html='''<!DOCTYPE html>
 <html>
 <head><title> PRUEBA DE DESARROLLO INICIAL </title></head>
@@ -20,15 +21,17 @@ html='''<!DOCTYPE html>
 <button name="LEDb" value='OFF' type='submit'> LED OFF</button>
 <br><br>
 '''
-
+#Declaracion del pin que enciende el led.
 led0=machine.Pin(0,machine.Pin.OUT)
-led0.value(0)
+led0.value(0) #se deja el led apagado
 
+#Declaracion del socket
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('',80))
 s.listen(5)
 
 while True:
+	
 	conn,addr=s.accept()
 	print("se logro comunicar desde %s" % str(addr))
 	request=conn.recv(1024)
